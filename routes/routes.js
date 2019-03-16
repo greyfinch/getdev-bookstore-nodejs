c275a22174b2c8a9e674e86a0df4204ab0dc9cb2
Ikenna <onyejiaka.samuel@gmail.com>
2019-03-17 00:44:08 +0100

    Add comments to routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,17 +5,32 @@ const UserController = require('../controllers/user-controller');
 const CategoryController = require('../controllers/book-category-controller');
 const BookController = require('../controllers/book-controller');
 
-//define App routes
+/*
+    * Define Application Routes
+*/
+
+//Home Page
 routes.get('/', (req, res) => { return res.send('Welcome to getDev Book Store') });
-routes.get('/users', UserController.allUsers)
+//Retrive All users
+routes.get('/users', UserController.allUsers);
+//Create a new user
 routes.post('/createuser', UserController.createUser);
-routes.get('/bookcategories', CategoryController.bookCategories)
+//Retrive all book categories
+routes.get('/bookcategories', CategoryController.bookCategories);
+//Add a new Category of book
 routes.post('/addbookcategory', CategoryController.addBookCategory);
+//Retrive all added book
 routes.get('/books', BookController.books);
+//Add a new book
 routes.post('/addbook', BookController.addBook);
+//Update the details of a book
 routes.put('/updatebook', BookController.updateBook);
+//Delete a book and all associated data to it
 routes.delete('/deletebook', BookController.deleteBook);
+//Add a new stock for any book
 routes.post('/addstock', BookController.addBookStock);
+//Rate a book
 routes.post('/ratebook', BookController.rateBook);
 
+//Make route module accessible from other modules
 module.exports = routes;
